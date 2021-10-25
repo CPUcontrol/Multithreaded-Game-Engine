752e1cbf07ac7d19e2c651d0b19e6ffeb47df7f9
Eric Jing <rune.dart4@gmail.com>
2021-10-25 08:31:30 -0400

    add compatibility mask for opengl

--- src/main.cpp
+++ src/main.cpp
@@ -272,6 +272,7 @@ int main(int argc, char **argv){
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetSwapInterval(1);
 
     mdata.wind = SDL_CreateWindow(

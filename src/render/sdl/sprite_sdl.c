1db20f7a9a0beac01b5e22c67f6a8355b35d6f3f
Eric Jing <rune.dart4@gmail.com>
2022-07-13 19:22:48 -0700

    remove sdl backend files

--- src/render/sdl/sprite_sdl.c
+++
@@ -1,7 +0,0 @@
-#include "sprite_sdl.h"
-#include "../../core/allocator.h"
-
-void Enj_Sprite_OnFree_SDL(void *d, void *ctx, Enj_Allocator *a){
-    Enj_Sprite_SDL *sp = (Enj_Sprite_SDL *)d;
-    Enj_Free(a, sp);
-}
